dac0cbca2993d5c94a4a2e3417ec8ae139faa6c3
Todd Crone <todd.crone@perceptivesoftware.com>
2014-06-05 17:03:47 -0400

    Add postgres tweaks.

--- db/schema.rb
+++ db/schema.rb
@@ -13,6 +13,9 @@
 
 ActiveRecord::Schema.define(version: 20140605164258) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "items", force: true do |t|
     t.string   "title"
     t.integer  "priority"
