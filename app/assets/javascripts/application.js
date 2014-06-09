2fb03b2bdf17e483596a2360e8a700b70bbc55a1
Todd Crone <todd.crone@perceptivesoftware.com>
2014-06-09 01:37:40 -0400

    Fix angular inclusion.

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -12,5 +12,5 @@
 //
 //= require jquery
 //= require jquery_ujs
-//= require turbolinks
+//= require angular.mini
 //= require_tree .
--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -1,2 +1,2 @@
-Rails.application.config.assets.precompile += %w( itemcontroller.js )
 Rails.application.config.assets.precompile += %w( angular.mini.js )
+Rails.application.config.assets.precompile += %w( itemcontroller.js )
